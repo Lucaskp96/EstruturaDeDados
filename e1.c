84275f86f51995532808740320db842f35aaa452
Lucas Kuster <lucaskusterp@hotmail.com>
2019-03-18 22:45:45 -0300

    Versão final do exercício 1

--- e1.c
+++ e1.c
@@ -7,13 +7,15 @@ int main(){
   int* tamanho_vetor;
   int* vetor;
 	int* auxiliar;
+
   tamanho_vetor = (int*)malloc(sizeof(int));
   printf("Digite o tamanho do vetor: ");
   scanf("%d", tamanho_vetor);
   vetor = (int*)malloc((*tamanho_vetor)*sizeof(int));
 	auxiliar = vetor;
-printf("auxiliar:%p\n", auxiliar);
+
   preencher_vetor(vetor,(*tamanho_vetor));
+
   printf("\n");
   do{
     printf("%d ", *vetor);
@@ -21,17 +23,11 @@ printf("auxiliar:%p\n", auxiliar);
     (*tamanho_vetor)--;
   }while((*tamanho_vetor) > 0);
   printf("\n");
-	printf("tm:%p\n", tamanho_vetor);
+
   free(tamanho_vetor);
-	printf("tm1:%p\n", tamanho_vetor);
 	tamanho_vetor = NULL;
-	printf("tm2:%p\n", tamanho_vetor);
-	printf("auxiliar:%p\n", auxiliar);
   free(auxiliar);
-	printf("auxiliar1:%p\n", auxiliar);
 	auxiliar = NULL;
-	printf("auxiliar2:%p\n", auxiliar);
-	printf("vetor:%p\n", vetor);
 
   return 0;
 }
@@ -39,12 +35,16 @@ printf("auxiliar:%p\n", auxiliar);
 
 void preencher_vetor(int* vetor, int tamanho){
   int* inicio_vetor;
-  inicio_vetor = (int*)vetor;
+  inicio_vetor = vetor;
   printf("Digite os elementos para o vetor:\n");
   do{
     scanf("%d", vetor);
     vetor++;
 	tamanho--;
   }while(tamanho > 0);
-  //return (int*)inicio_vetor;
 }
+
+/* Fazer um programa que leia um valor N e crie dinamicamente um vetor de N elementos.
+Passe esse vetor para uma função que vai ler os elementos para o vetor.
+No programa principal, imprima o vetor. Além disso, antes de finalizar o programa,
+deve-se liberar a área de memória alocada. */
