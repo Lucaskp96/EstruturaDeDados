722ecf2bc56bd03fb2e62c7dcbb17cf869d44746
Lucas Kuster <lucaskusterp@hotmail.com>
2019-03-23 21:12:52 -0300

    Algumas alterações

--- e5.c
+++ e5.c
@@ -2,27 +2,28 @@
 #include <stdio.h>
 
 typedef struct person{
-  char nome[20];
-  int data_nascimento;
-  int cpf;
+  char nome[30];
+  char data_nascimento[12];
+  char cpf[12];
 } Pessoa;
 
 Pessoa* inicia_estrutura(Pessoa* pessoa){
   pessoa = (Pessoa*) malloc(sizeof(Pessoa));
   printf("Nome: ");
-  fgets(pessoa->nome,20,stdin);
+  fgets(pessoa->nome,30,stdin);
   printf("Data de nascimento: ");
-  scanf("%d", &pessoa->data_nascimento);
+  fgets(pessoa->data_nascimento,12,stdin);
   printf("CPF: ");
-  scanf("%d", &pessoa->cpf);
+  fgets(pessoa->cpf,12,stdin);
+
   return pessoa;
 }
 
 void imprime(Pessoa* pessoa){
   printf("\nNome: ");
   printf("%s", pessoa->nome);
-  printf("Data de Nascimento: %d\n", pessoa->data_nascimento);
-  printf("CPF: %d\n", pessoa->cpf);
+  printf("Data de Nascimento: %s", pessoa->data_nascimento);
+  printf("CPF: %s\n", pessoa->cpf);
 }
 
 int main(){
