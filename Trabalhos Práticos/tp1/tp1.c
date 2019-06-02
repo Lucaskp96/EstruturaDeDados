6da7e520dc053802cf85f26c02e408374d04f751
Lucas Kuster <lucaskusterp@hotmail.com>
2019-06-02 20:43:33 -0300

    Inserção dos trabalhos práticos

---
+++ Trabalhos Práticos/tp1/tp1.c
@@ -0,0 +1,147 @@
+// Nao se preocupem com isso
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define RESET   "\x1b[0m"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define uteis
+#define OFFSET 97
+#define ALFABETO 26
+#define true 1
+#define false 0
+#define bool int
+#define DEFAULT "Traducao desconhecida\0"
+
+// Estruturas de dados
+struct Celula {
+    char * pais;
+    char * frase;
+};
+
+struct Dicionario {
+    struct Celula letras[ALFABETO];
+};
+
+typedef struct Celula Celula;
+typedef struct Dicionario Dicionario;
+
+// Pesquisar : retorna true se o pais esta no dicionario, false caso contrario
+bool pesquisar(Dicionario * dicionario, char * pais) {
+    if (dicionario->letras[pais[0] - OFFSET].pais) {
+        if (strcmp(pais, dicionario->letras[pais[0] - OFFSET].pais) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Inserir : insere par pais - frase no dicionario caso a primeira letra do pais esteja livre e retorna true, false caso nao insira
+bool inserir(Dicionario * dicionario, char * pais, char * frase) {
+    if(pesquisar(dicionario,pais) == false){
+        dicionario->letras[pais[0] - OFFSET].pais = pais;
+        dicionario->letras[pais[0] - OFFSET].frase = frase;
+        return true;
+    }else{
+        return false;
+    }
+}
+
+// Saudar : retorna a a traducao de "Feliz Natal!" no pais passado como parametro caso esteja no dicionario, retorna frase DEFAULT caso contrario
+char * saudar(Dicionario * dicionario, char * pais) {
+    if(pesquisar(dicionario,pais) == true){
+        return dicionario->letras[pais[0] - OFFSET].frase;
+    }else{
+        return DEFAULT;
+    }
+}
+
+// Inicializa dicionario
+Dicionario * initDicionario() {
+    Dicionario * d = (Dicionario *) malloc(sizeof(Dicionario));
+
+    for (int i = 0; i < ALFABETO; i++) {
+        d->letras[i].pais == NULL;
+    }
+
+    printf("Inserindo brasil\n");
+    inserir(d, "brasil\0", "Feliz Natal!\0");
+
+    printf("Inserindo alemanha\n");
+    inserir(d, "alemanha", "Frohliche Weihnachten!\0");
+
+    printf("Inserindo coreia\n");
+    inserir(d, "coreia", "Chuk Sung Tan!\0");
+
+    printf("Inserindo grecia\n");
+    inserir(d, "grecia", "Kala Christougena!\0");
+
+    printf("Inserindo estados-unidos\n");
+    inserir(d, "estados-unidos", "Merry Christmas!\0");
+
+    printf("Inserindo portugal\n");
+    inserir(d, "portugal", "Feliz Natal!\0");
+
+    printf("Inserindo suecia\n");
+    inserir(d, "suecia", "God Jul!");
+
+    printf("Inserindo turquia\n");
+    inserir(d, "turquia", "Mutlu Noeller\0");
+
+    printf("Inserindo mexico\n");
+    inserir(d, "mexico", "Feliz Navidad!\0");
+
+    printf("Inserindo italia\n");
+    inserir(d, "italia", "Buon Natale!\0");
+
+    printf("Inserindo libia\n");
+    inserir(d, "libia", "Buon Natale!\0");
+
+    printf("Inserindo japao\n");
+    inserir(d, "japao", "Merii Kurisumasu!\0");
+
+    return d;
+}
+
+int main() {
+    printf("---Inicializacao---\n");
+    Dicionario * d = initDicionario();
+    char * resultado;
+
+    printf("\n---Testes de Validacao---\n");
+    printf("1. Pesquisa por traducao existente");
+    if (pesquisar(d, "brasil")) {
+        printf(GREEN " PASS!\n" RESET);
+    }
+    else {
+        printf(RED " FAIL.\n" RESET);
+    }
+
+    printf("2. Pesquisa por traducao nao existente");
+    if (pesquisar(d, "bolivia") == false) {
+        printf(GREEN " PASS!\n" RESET);
+    }
+    else {
+        printf(RED " FAIL.\n" RESET);
+    }
+
+    printf("3. Saudacao valida");
+    if (strcmp(saudar(d, "japao"), "Merii Kurisumasu!") == 0) {
+        printf(GREEN " PASS!\n" RESET);
+    }
+    else {
+        printf(RED " FAIL.\n" RESET);
+    }
+
+    printf("4. Saudacao invalida");
+    if (strcmp(saudar(d, "marrocos"), DEFAULT) == 0) {
+        printf(GREEN " PASS!\n" RESET);
+    }
+    else {
+        printf(RED " FAIL.\n" RESET);
+    }
+
+    return 0;
+}
